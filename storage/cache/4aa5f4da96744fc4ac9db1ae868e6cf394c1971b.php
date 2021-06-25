a88bd21f7fe5f5247a8e49a006bc0f5e4b591b58
Dsprog <dsprog@gmail.com>
2021-06-25 01:06:16 -0300

    config erro custom

---
+++ storage/cache/4aa5f4da96744fc4ac9db1ae868e6cf394c1971b.php
@@ -0,0 +1,8 @@
+
+<?php $__env->startSection('content'); ?>
+    <div style="padding:20px">
+        <?php echo e($name); ?>
+
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\sites\CleanCodeStudioSlim4Auth\resources\views/auth/home.blade.php ENDPATH**/ ?>
